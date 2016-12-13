b846f97bc40cddb9f05d3bff9429e9777e06cb94
John <johnwilles@gmail.com>
2016-12-12 19:44:02 -0500

    added /todos filter db funtionality

--- server.js
+++ server.js
@@ -16,27 +16,56 @@ app.get('/', function (req, res) {
 
 // GET /todos
 app.get('/todos', function (req, res) {
-	var queryParams =req.query;
-	var filteredTodos = todos;
+	var query = req.query
+	var where = {};
+
+	if (query.hasOwnProperty('completed') && query.completed === "true") {
+		where.completed = true;
+	} else if (query.hasOwnProperty('completed') && query.completed === "false") {
+		where.completed = false;
+	}
+
+	if (query.hasOwnProperty('q') && query.q.length > 0) {
+		where.description = { $like: '%' + query.q + '%'};
+	}
 	
-	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-		filteredTodos  = _.where(todos, { completed: true})
-	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-			filteredTodos  = _.where(todos, { completed: false})
-	}
+	db.todo.findAll({where: where}).then(function (todos) {
+		if (!todos) {
+			return res.status(404).send();
+		}	
+	//	console.log(todos)	
+		return res.json(todos);	
+	}, function (e) {
+		res.status(500).send();
+	});	
 
-	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-		filteredTodos  = _.filter(todos, function (todo) {
-				return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-		});
-	}
-
-
-	if (!filteredTodos) {
-		return res.status(400).send();
-	} 
-
-	res.json(filteredTodos);
+//		return Todo.findAll({
+//			where: {
+//				description: {
+//					$like: '%Mail%'
+//				}
+//			}	
+//		})
+//	var filteredTodos = todos;
+//	
+//	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+//		filteredTodos  = _.where(todos, { completed: true})
+//	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+//			filteredTodos  = _.where(todos, { completed: false})
+//	}
+//
+//	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+//		filteredTodos  = _.filter(todos, function (todo) {
+//				return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+//		});
+//	}
+//
+//
+//	if (!filteredTodos) {
+//		return res.status(400).send();
+//	} 
+//
+//	res.json(filteredTodos);
 });
 
 // GET /todos/:id
@@ -46,7 +75,7 @@ app.get('/todos/:id', function (req, res) {
 		if (!todo) {
 			return res.status(404).send();
 		}
-			return res.json(todo.toJSON());
+		return res.json(todo.toJSON());
 	}, function (error) {
 		res.status(500).send();
 	});
